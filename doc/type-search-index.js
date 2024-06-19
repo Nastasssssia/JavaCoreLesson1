0497b58ec2ada811adc5e3a1860e3cf0da7f5647
Nastassia <anastasia.goroninova@icloud.com>
2024-06-19 21:01:41 +0300

    add doc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.gb.goroninova","l":"Decorator"},{"p":"ru.gb.goroninova","l":"Main"},{"p":"ru.gb.goroninova","l":"OtherClass"}]
